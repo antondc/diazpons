10ea8919e8f2703d3b4df4bcc1cf42ba35d6a7ec
antoniodcorrea <hello@antoniodiaz.me>
2025-06-01 22:45:32 +0200

    DP-01 basic header and loading images

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,12 +5,14 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const WEBPACK_BUILD = path.resolve(process.cwd(), "webpack");
 const WEBPACK_DIST = path.resolve(process.cwd(), "dist");
 const WEBPACK_SRC = path.resolve(process.cwd(), "src");
 const WEBPACK_PRESENTATION = path.resolve(WEBPACK_SRC, "presentation");
 const WEBPACK_SRC_CLIENT = path.resolve(WEBPACK_BUILD, "index.js");
+const WEBPACK_ASSETS = path.resolve(WEBPACK_PRESENTATION, "assets");
 
 module.exports = {
   entry: WEBPACK_SRC_CLIENT,
@@ -70,5 +72,19 @@ module.exports = {
       chunkFilename: "[name]-[fullhash:4].css",
     }),
     new CaseSensitivePathsPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.join(WEBPACK_ASSETS, "img"),
+        to: path.join(WEBPACK_DIST, "img"),
+      },
+      {
+        from: path.join(WEBPACK_ASSETS, "svg"),
+        to: path.join(WEBPACK_DIST, "svg"),
+      },
+      {
+        from: path.join(WEBPACK_ASSETS, "favicon"),
+        to: path.join(WEBPACK_DIST, "favicon"),
+      },
+    ]),
   ],
 };
