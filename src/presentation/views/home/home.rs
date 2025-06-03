09c71b22bc66d8f109a2e281a99ad3135876ecba
antoniodcorrea <hello@antoniodiaz.me>
2025-06-03 19:23:21 +0200

    DP-01 rendering svg

--- src/presentation/views/home/home.rs
+++ src/presentation/views/home/home.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use crate::domain::Book;
 use askama::Template;
 use axum::response::Html;
@@ -8,12 +10,23 @@ use axum::response::Html;
 struct HomeTemplate<'a> {
     title: String,
     books: &'a [Book],
+    twitter_svg: String,
+    facebook_svg: String,
+    instagram_svg: String,
 }
 
 pub async fn home_view(books: Vec<Book>) -> Html<String> {
+    let twitter_svg = fs::read_to_string("src/presentation/assets/svg/twitter-logo.svg").unwrap();
+    let facebook_svg = fs::read_to_string("src/presentation/assets/svg/facebook-logo.svg").unwrap();
+    let instagram_svg =
+        fs::read_to_string("src/presentation/assets/svg/instagram-logo.svg").unwrap();
+
     let template = HomeTemplate {
         title: String::from("Sci-Fi Books"),
         books: &books,
+        twitter_svg,
+        facebook_svg,
+        instagram_svg,
     };
 
     Html(template.render().unwrap())
