34e6259a66a322d5a0496cc1e890b2377ea3dd5e
antoniodcorrea <hello@antoniodiaz.me>
2025-06-26 14:34:23 +0200

    DP-01 menu

--- src/presentation/components/header/header.ts
+++ src/presentation/components/header/header.ts
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Header JavaScript loaded.");
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Header JavaScript loaded.');
+
+  const menu = document.getElementById('header-menu');
+  const header = document.getElementById('header');
+  menu?.addEventListener('click', () => {
+    const headerIsOpen = header?.classList.contains('Header--open');
+    if (headerIsOpen) {
+      header?.classList.remove('Header--open');
+    } else {
+      header?.classList.add('Header--open');
+    }
+  });
 });
