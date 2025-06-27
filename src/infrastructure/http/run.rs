49eb6288e53c1d868ce5307c7728ceaae21de0d0
antoniodcorrea <hello@antoniodiaz.me>
2025-06-27 21:53:36 +0200

    DP-01 run http or https depending on environment

--- src/infrastructure/http/run.rs
+++ src/infrastructure/http/run.rs
@@ -1,7 +1,8 @@
 use super::constants::{HTTP_PORT, IP_STRING, TLS_CERTIFICATE_PATH, TLS_KEY_PATH};
 use super::routes;
 use crate::constants::STATIC_FILES_PATH;
-use crate::infrastructure::http::routes::{about, authors,author, book, books, catchers, home, press, serie};
+use crate::infrastructure::http::routes::{about, author, authors, book, books, catchers, home, press, serie};
+use crate::infrastructure::http::types::Environments;
 use crate::shared::utils::parse_ip;
 use rocket::config::{CipherSuite, Config, TlsConfig};
 extern crate dotenv;
@@ -11,6 +12,7 @@ use std::path::Path;
 
 #[rocket::main]
 pub async fn main() {
+  let environment = dotenv::var("ENVIRONMENT").unwrap_or_else(|_| Environments::Development.to_string());
   let parsed_ip = parse_ip(IP_STRING).unwrap();
 
   // Check that the TLS certificate and key files exists to run on https;
@@ -18,13 +20,18 @@ pub async fn main() {
   let tls_certificate_file_exits = tls_certificate_path.exists();
   let tls_key_path = Path::new(TLS_KEY_PATH);
   let tls_key_file_exits = tls_key_path.exists();
-  let tls_config: Option<TlsConfig> = match (tls_certificate_file_exits, tls_key_file_exits) {
-    (true, true) => Some(
-      TlsConfig::from_paths(TLS_CERTIFICATE_PATH, TLS_KEY_PATH)
-        .with_ciphers(CipherSuite::TLS_V13_SET)
-        .with_preferred_server_cipher_order(true),
-    ),
-    _ => None,
+  let tls_config: Option<TlsConfig> = if environment == Environments::Development.to_string() {
+    match (tls_certificate_file_exits, tls_key_file_exits) {
+      (true, true) => Some(
+        TlsConfig::from_paths(TLS_CERTIFICATE_PATH, TLS_KEY_PATH)
+          .with_ciphers(CipherSuite::TLS_V13_SET)
+          .with_preferred_server_cipher_order(true),
+      ),
+      _ => None,
+    }
+  } else {
+    println!("[INFO] Running in development mode (HTTP only).");
+    None
   };
 
   let config = Config {
